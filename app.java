83b7a5480a50f9655b6d1d25983945870b9e8e49
soniyasony7654 <108574159+soniyasony7654@users.noreply.github.com>
2026-08-30 20:31:57 +0530

    Shorten prompt message for user input

--- app.java
+++ app.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class CheckEvenOdd {
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
-		System.out.print("Enter a number: ");
+		System.out.print("Enter a num: ");
 		int num = reader.nextInt();
 		if (num % 2 == 0)
 			System.out.println(num + " is even");
